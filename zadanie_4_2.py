1f5ab81b0895d751cdca9688eee20a82ecdb51b7
Stanislaw Szwiec <szwiec.st@jacobs.com>
2022-03-30 17:51:28 +0200

    Simplify the function

--- zadanie_4_2.py
+++ zadanie_4_2.py
@@ -7,10 +7,7 @@ def check_if_palindrome(word):
         Argument:
         word  
     """
-    a=0
     word=''.join(filter(str.isalnum,(word.replace(' ', '')).lower()))
-    for i in range(0,len(word)):
-        if word[i]==word[len(word)-i-1]:
-            a=a+1
-    return a==len(word)
+    return word==word[::-1]
 print(check_if_palindrome('0A to Kamela, ale ,,ma kota.0'))
+
