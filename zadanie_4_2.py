eade1776065e5cab86960efe2ff1bb419c74ff41
Stanislaw Szwiec <szwiec.st@jacobs.com>
2022-03-30 11:08:25 +0200

    Add help for function

--- zadanie_4_2.py
+++ zadanie_4_2.py
@@ -1,12 +1,14 @@
 
 import math
-def check_if_palindrom1(word):
+def check_if_palindrome(word):
+    """
+        Based on argument passed, checks if given word is palindrome.
+        Argument:
+        word  
+    """
     a=0
     for i in range(0,math.floor(len(word)/2)):
         if word[i]==word[len(word)-i-1]:
             a=+1
     print(a==math.floor(len(word)/2))
-check_if_palindrom1('kaatak')
-
-
-
+check_if_palindrome('kaatak')
